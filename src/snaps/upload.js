cfb38886cc4903084703dc47ca2965a779fd527b
Yashwant Gaikawad <yashwant.gaikawad@gmail.com>
2017-09-06 06:21:35 +0530

    snap upload endpoint added

--- src/snaps/upload.js
+++ src/snaps/upload.js
@@ -3,7 +3,7 @@ const auth = require('../shared/auth');
 const dynamodb = require('../shared/dynamodb');
 const aws = require('aws-sdk');
 const uuid = require('uuid');
-function get_sign_s3(event, context, callback) {
+function upload(event, context, callback) {
   if (!event || !event.headers) {
     callback({
       error: 'Not a valid event object'
@@ -42,15 +42,17 @@ function handleRequest(user, event, callback) {
 
     if(result.Items.length!=0)
     {
+        let body = JSON.parse(event.body);
+        let image_body = body.imageSrc;
         var user_id = result.Items[0].id;
-        var s3 = new aws.S3();
+        var s3 = new aws.S3( { params: {Bucket: process.env.USER_IMAGES_S3_BUCKET} } );
         var filetype = "image/jpeg";
         let uuidv4 = require('uuid/v4');
-        let filename = uuidv4() + '.jpeg';
-        var params = {
-            Bucket: process.env.USER_IMAGES_S3_BUCKET,
-            Key: filename,
-            Expires: 60,
+        let filename = uuidv4();
+        var data = {
+            Key: filename+'.jpeg',
+            Body: image_body,
+            ContentEncoding: 'base64',
             ContentType: filetype
         };
 
@@ -59,7 +61,7 @@ function handleRequest(user, event, callback) {
             secretAccessKey: process.env.S3_BUCKET_AWS_SECRET_KEY
         });
         
-        s3.getSignedUrl('putObject', params, function(err, data) {
+        s3.putObject(data, function(err, data) {
             if (err) {
                 console.log(err);
                 const response = {
@@ -71,7 +73,7 @@ function handleRequest(user, event, callback) {
             } else {
                 const response = {
                   statusCode: 200,
-                  body: JSON.stringify({'url':data, 'filename':filename, 'filetype':filetype}),
+                  body: JSON.stringify({'filename':filename+'.jpeg', 'filetype':filetype}),
                 };
                 callback(null, response);
                 return;
@@ -92,4 +94,4 @@ function handleRequest(user, event, callback) {
   });
 }
 
-module.exports.get_sign_s3 = get_sign_s3;
+module.exports.upload = upload;
